17a0b111f96ac49e425877dd3f43f73a439ec80b
Asus VivoBook <novacntikafitri@gmail.com>
2022-04-09 11:38:12 +0700

    Initial Commit Event Handling example

---
+++ app/src/main/java/com/example/eventhandlingexample/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.eventhendlingexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.eventhandlingexample.R
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnDisplayMessage.setOnClickListener {
+            Toast.makeText(
+                applicationContext ,
+                "Hello World" ,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+        btnDisplayMessage.setOnLongClickListener {
+        Snackbar.make(root_layout, "Long click",Snackbar.LENGTH_LONG).show()
+        true
+    }
+
+}
+}
